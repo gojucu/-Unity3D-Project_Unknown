c543201c728b035fb5ee5735775c5e43b6baab3e
FurkanÇetinkaya <gojucu@gmail.com>
2019-03-02 19:51:05 +0300

    First part (footsteps, Sea, etc.)

---
+++ Project Unknown/Assets/_CameraUI/CursorAffordance.cs
@@ -0,0 +1,44 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+
+//namespace RPG.CameraUI
+//{
+//    [RequireComponent(typeof(CameraRaycaster))]
+//    public class CursorAffordance : MonoBehaviour
+//    {
+        
+//        [SerializeField] Texture2D targetCursor = null;
+//        [SerializeField] Texture2D unkownCursor = null;
+//        [SerializeField] Vector2 cursorHotspot = new Vector2(0, 0);
+
+//        [SerializeField] const int walkableLayerNumber = 9;
+//        [SerializeField] const int enemyLayerNumber = 10;
+//        // [SerializeField] int walkableLayerNumber = 11;
+
+//        CameraRaycaster cameraRaycaster;
+
+//        // Use this for initialization
+//        void Start()
+//        {
+//            cameraRaycaster = GetComponent<CameraRaycaster>();
+//            cameraRaycaster.notifyLayerChangeObservers += OnLayerChanged;
+//        }
+
+//        void OnLayerChanged(int newLayer)
+//        {
+//            switch (newLayer)
+//            {
+                
+//                case enemyLayerNumber:
+//                    Cursor.SetCursor(targetCursor, cursorHotspot, CursorMode.Auto);
+//                    break;
+//                default:
+//                    Cursor.SetCursor(unkownCursor, cursorHotspot, CursorMode.Auto);
+
+//                    return;
+//            }
+//        }
+//    }
+//}
---
+++ Project Unknown/Assets/_Characters/Scripts/DominantHand.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Characters { 
+public class DominantHand : MonoBehaviour {
+    // add to dominant hand on player character prefab
+     
+}
+}
